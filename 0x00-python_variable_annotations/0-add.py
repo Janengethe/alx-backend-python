6efb403deea38f94c13a1b3b8581b6413504add8
Janengethe <janengethew@gmail.com>
2022-04-14 13:13:12 +0300

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+Returns sum of float
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Takes float as arguments
+    Returns a float
+    """
+    return(a + b)
